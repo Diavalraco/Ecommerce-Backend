ade6cd27582a99638bb8c4c61bc8fce950ef29d5
Aarushyadav <akaaarushyadav786@gmail.com>
2025-08-12 00:22:55 +0530

    Addons

--- src/microservices/sign.js
+++ src/microservices/sign.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 const config = require("../config/config");
 
-const orderId = "order_R47jbSX18F5k4C";
+const orderId = "order_R48FRabQ7HGoIy";
 const paymentId = "pay_ABCdef012345";
 const keySecret = config.razorpay.key_secret;
 
@@ -11,6 +11,3 @@ const signature = crypto
   .digest("hex");
 
 console.log("Signature:  ", signature);
-  
-RAZORPAY_KEY_ID=rzp_test_7f2a3rN8kKecHB
-RAZORPAY_KEY_SECRET=XYLikb9n3MxIToj6I30UFkxd
